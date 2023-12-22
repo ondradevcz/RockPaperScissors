16cdb26bee7b9778a7ca18db1cfb44c1f9e19407
Ondrej Toman <126340449+ondradevcz@users.noreply.github.com>
2023-12-22 18:41:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,26 @@
 import random
 
-# Creating variable
-guess = ""
+def RockPaperScissors():
+    types_of_guesses = ('Rock', 'Paper', 'Scissors')
 
-# Start of the game
-print('Welcome to Rock Paper Scissors! \n If you want to end the game, type "end"')
+    while True:
+        guess = input('Type your guess  \n Rock, Paper or Scissors?  \n       Type here: ')
 
+        if guess.lower() == "end":
+            print('Thanks for playing with me!')
+            break
+        elif guess not in types_of_guesses:
+            print('Invalid guess')
+        else:
+            computer_guess = random.choice(types_of_guesses)
 
-# Game Function
-def Start():
-    global guess  # Use the global variable
+            if guess == computer_guess:
+                print("It's a tie!")
+            elif (guess == "Rock" and computer_guess == "Scissors") or \
+                    (guess == "Scissors" and computer_guess == "Paper") or \
+                    (guess == "Paper" and computer_guess == "Rock"):
+                print("You win!")
+            else:
+                print('You lost')
 
-    guess = input('Type your guess  \n Rock, Paper or Scissors?  \n       Type here: ')
-    return guess
-
-
-Start()
-
-types_of_guesses = ('Rock', 'Paper', 'Scissors')
-computer_guess = random.choice(types_of_guesses)
-
-if guess.lower() == "end":
-    print('Thanks for playing with me!')
-else:
-    if guess == computer_guess:
-        print("It's a tie!")
-        Start()
-    elif (guess == "Rock" and computer_guess == "Scissors") or \
-            (guess == "Scissors" and computer_guess == "Paper") or \
-            (guess == "Paper" and computer_guess == "Rock"):
-        print("You win!")
-        Start()
-    elif guess in types_of_guesses:
-        print('You lost')
-        Start()
-    else:
-        print('Invalid guess')
-        Start()
-
+RockPaperScissors()
